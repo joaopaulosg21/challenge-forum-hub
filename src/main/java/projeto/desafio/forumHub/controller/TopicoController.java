586fb081372ba8771461cb65487f85bff24e9497
joaopaulo <joaopaulosg17@gmail.com>
2024-06-12 09:53:40 -0300

    feat: Adicionado metodo para buscar todos os topicos usando paginação e mudança no campo data_criacao da tabela topicos

--- src/main/java/projeto/desafio/forumHub/controller/TopicoController.java
+++ src/main/java/projeto/desafio/forumHub/controller/TopicoController.java
@@ -2,12 +2,13 @@ package projeto.desafio.forumHub.controller;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
 import projeto.desafio.forumHub.domain.topico.DadosRegistroTopico;
 import projeto.desafio.forumHub.domain.topico.DadosTopico;
@@ -15,6 +16,7 @@ import projeto.desafio.forumHub.domain.topico.Topico;
 import projeto.desafio.forumHub.domain.topico.TopicoRepository;
 
 import java.net.URI;
+import java.util.List;
 
 @RestController
 @RequestMapping("/topicos")
@@ -24,9 +26,16 @@ public class TopicoController {
 
     @PostMapping
     @Transactional
-    public ResponseEntity<?> registrar(@Valid @RequestBody DadosRegistroTopico dados, UriComponentsBuilder builder) {
+    public ResponseEntity<DadosTopico> registrar(@Valid @RequestBody DadosRegistroTopico dados, UriComponentsBuilder builder) {
         Topico topico = topicoRepository.save(new Topico(dados));
         URI uri = builder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
         return ResponseEntity.created(uri).body(new DadosTopico(topico));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<?>> buscarTodos(@PageableDefault(size = 10,sort = {"dataCriacao"},direction = Sort.Direction.ASC)
+                                                             Pageable pageable) {
+        var dadosTopicos = topicoRepository.findAll(pageable).map(DadosTopico::new);
+        return ResponseEntity.ok(dadosTopicos);
     }
 }
