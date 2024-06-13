f13f822fa3dcda206cd951bf97a61339e1f53a32
joaopaulo <joaopaulosg17@gmail.com>
2024-06-13 10:56:44 -0300

    feat: Adicionada entity Usuario e metodos para criar novos usuarios

--- src/main/java/projeto/desafio/forumHub/infra/security/SecurityConfiguration.java
+++ src/main/java/projeto/desafio/forumHub/infra/security/SecurityConfiguration.java
@@ -6,6 +6,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -18,5 +20,10 @@ public class SecurityConfiguration {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
