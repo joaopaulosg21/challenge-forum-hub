d98be6bfd3d8f668869348436b3073d14d383327
joaopaulo <joaopaulosg17@gmail.com>
2024-06-13 19:34:40 -0300

    feat: Adicionada documentação para a api

--- src/main/java/projeto/desafio/forumHub/infra/security/SecurityConfiguration.java
+++ src/main/java/projeto/desafio/forumHub/infra/security/SecurityConfiguration.java
@@ -1,5 +1,9 @@
 package projeto.desafio.forumHub.infra.security;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -30,6 +34,7 @@ public class SecurityConfiguration {
                 .authorizeHttpRequests(request -> {
                    request.requestMatchers(HttpMethod.POST,"/usuarios").permitAll()
                            .requestMatchers(HttpMethod.POST,"/login").permitAll()
+                           .requestMatchers(HttpMethod.GET,"/swagger-ui.html","/swagger-ui/**","/v3/api-docs/**").permitAll()
                            .anyRequest().authenticated();
                 }).addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class);
 
@@ -44,5 +49,19 @@ public class SecurityConfiguration {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().
+                        addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes
+                        ("Bearer Authentication", createAPIKeyScheme()));
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 }
